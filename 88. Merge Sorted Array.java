86cfc67e55cc61b71151b86f6148679c036d17a1
syzadele <syzadele@outlook.com>
2019-05-17 13:06:14 +0200

    Update 88. Merge Sorted Array.java

--- 88. Merge Sorted Array.java
+++ 88. Merge Sorted Array.java
@@ -1,7 +1,6 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        System.arraycopy(nums2, 0, nums2, 0, n);
-        System.arraycopy(nums2, 0, nums1, m, nums2.length);
+        System.arraycopy(nums2, 0, nums1, m, n);
         Arrays.sort(nums1);
     }
 }
