fc18bff1c864375cbcbac3ef67e28c61e26a333b
syzadele <syzadele@outlook.com>
2019-05-24 15:07:03 +0200

    Create 189. Rotate Array.java

---
+++ 189. Rotate Array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        if (nums.length > 1) {
+            k = k % nums.length;
+            int[] temp = new int[k];
+            System.arraycopy(nums, nums.length - k, temp, 0, k);
+            System.arraycopy(nums, 0, nums, k, nums.length - k);
+            System.arraycopy(temp, 0, nums, 0, k);        
+        }
+        
+    }
+}
+
